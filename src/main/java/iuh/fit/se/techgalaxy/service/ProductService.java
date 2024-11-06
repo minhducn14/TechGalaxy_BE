19359a58e3068bce782e14e1f2e25c713f8efc75
Kha <minhkha20031996@gmail.com>
2024-11-06 14:56:55 +0700

    commit init product base serice

---
+++ src/main/java/iuh/fit/se/techgalaxy/service/ProductService.java
@@ -0,0 +1,12 @@
+package iuh.fit.se.techgalaxy.service;
+
+import iuh.fit.se.techgalaxy.entities.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+}
+
