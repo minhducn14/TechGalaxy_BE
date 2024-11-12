1b018b4f1e7ac4f70cb302aea073b8f779090aea
GGDuck <vunguyenminhduc1410@gmail.com>
2024-11-12 19:47:00 +0700

    Read File From Path
    http://localhost:8081/storage/test/1731414681130-Test.jpg

---
+++ src/main/java/iuh/fit/se/techgalaxy/config/StaticResourcesWebConfiguration.java
@@ -0,0 +1,33 @@
+package iuh.fit.se.techgalaxy.config;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class StaticResourcesWebConfiguration
+        implements WebMvcConfigurer {
+
+    @Value("${upload-file.base-uri}")
+    private String uploadDir;
+    private String baseURI ;
+    @PostConstruct
+    public void init() {
+        Path uploadPath = Paths.get(uploadDir).toAbsolutePath();
+        this.baseURI = uploadPath.toString();
+        this.baseURI =  this.baseURI.replace("\\", "/");
+        this.baseURI = "file:///"+ this.baseURI+ "/";
+
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/storage/**")
+                .addResourceLocations(baseURI);
+    }
+}
