4ed66054c25c9a626cc50bc8694c1375ffe1f698
Tam <thanhtam0933019816@gmail.com>
2024-11-14 14:04:09 +0700

    cm

---
+++ src/main/java/iuh/fit/se/techgalaxy/dto/request/AttributeValueRequest.java
@@ -0,0 +1,15 @@
+package iuh.fit.se.techgalaxy.dto.request;
+
+import iuh.fit.se.techgalaxy.entities.ProductVariant;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AttributeValueRequest {
+	
+	
+}
