b66c85a264473c1375ffaddbd5c39ee43d885428
Kha <minhkha20031996@gmail.com>
2024-11-14 13:55:44 +0700

    commit create atr value request

--- src/main/java/iuh/fit/se/techgalaxy/controller/AttributeController.java
+++ src/main/java/iuh/fit/se/techgalaxy/controller/AttributeController.java
@@ -1,17 +1,12 @@
 package iuh.fit.se.techgalaxy.controller;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import iuh.fit.se.techgalaxy.dto.request.AttributeValueRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import iuh.fit.se.techgalaxy.dto.request.AttributeRequest;
 import iuh.fit.se.techgalaxy.dto.request.ProductRequest;
@@ -67,4 +62,11 @@ public class AttributeController {
 		return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
 	}
 
+	@PostMapping()
+	public ResponseEntity<DataResponse<ProductResponse>> createAttributeValueVariant(@RequestParam String provariant, @RequestBody List<AttributeValueRequest> request) {
+//		Set<ProductResponse> productRespone = new HashSet<>();
+//		productRespone.add(attributeServiceImpl.createProduct(request));
+	//	return ResponseEntity.ok(DataResponse.<ProductResponse>builder().data(productRespone).build());
+		return  null;
+	}
 }
