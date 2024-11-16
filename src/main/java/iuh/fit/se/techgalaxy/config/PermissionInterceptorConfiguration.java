98a29c1a2e28d5834d79e526e9d8b1bc6313aee7
GGDuck <vunguyenminhduc1410@gmail.com>
2024-11-16 22:40:38 +0700

    Update PermissionInterceptorConfiguration.java

--- src/main/java/iuh/fit/se/techgalaxy/config/PermissionInterceptorConfiguration.java
+++ src/main/java/iuh/fit/se/techgalaxy/config/PermissionInterceptorConfiguration.java
@@ -1,4 +1,4 @@
-package iuh.fit.se.techgalaxy.config;//package iuh.fit.se.techgalaxy.config;
+//package iuh.fit.se.techgalaxy.config;
 //
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
