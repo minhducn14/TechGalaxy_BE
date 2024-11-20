d34c185d5741e0101070c9610e4c6914a6dda5a4
Kha <minhkha20031996@gmail.com>
2024-11-20 21:36:07 +0700

    update ProductResponse

--- src/main/java/iuh/fit/se/techgalaxy/controller/AttributeController.java
+++ src/main/java/iuh/fit/se/techgalaxy/controller/AttributeController.java
@@ -4,7 +4,6 @@ import iuh.fit.se.techgalaxy.dto.request.AttributeRequest;
 import iuh.fit.se.techgalaxy.dto.request.AttributeValueRequest;
 import iuh.fit.se.techgalaxy.dto.response.AttributeResponse;
 import iuh.fit.se.techgalaxy.dto.response.DataResponse;
-import iuh.fit.se.techgalaxy.dto.response.ProductResponse;
 import iuh.fit.se.techgalaxy.dto.response.ValueResponse;
 import iuh.fit.se.techgalaxy.service.impl.AttributeServiceImpl;
 import lombok.AccessLevel;
@@ -13,61 +12,62 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RestController
 @RequestMapping("/attributes")
 public class AttributeController {
-	AttributeServiceImpl attributeServiceImpl;
-	
-	@GetMapping
-	public ResponseEntity<DataResponse<AttributeResponse>> getAllAttribute() {
-		return ResponseEntity
-				.ok(DataResponse.<AttributeResponse>builder().data(attributeServiceImpl.getAllAttribute()).build());
-	}
+    AttributeServiceImpl attributeServiceImpl;
 
-	@GetMapping("/attributes/{id}")
-	public ResponseEntity<DataResponse<AttributeResponse>> getAttributeById(@PathVariable String id) {
-		Set<AttributeResponse> attributeRespone = new HashSet<>();
-		attributeRespone.add(attributeServiceImpl.getAttributeById(id));
-		return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
-	}
+    @GetMapping
+    public ResponseEntity<DataResponse<AttributeResponse>> getAllAttribute() {
+        return ResponseEntity
+                .ok(DataResponse.<AttributeResponse>builder().data(attributeServiceImpl.getAllAttribute()).build());
+    }
 
-	@PostMapping
-	public ResponseEntity<DataResponse<AttributeResponse>> creatAttribute(@RequestBody AttributeRequest request) {
-		Set<AttributeResponse> attributeRespone = new HashSet<>();
-		attributeRespone.add(attributeServiceImpl.createAttribute(request));
-		return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
-	}
+    @GetMapping("/attributes/{id}")
+    public ResponseEntity<DataResponse<AttributeResponse>> getAttributeById(@PathVariable String id) {
+        Set<AttributeResponse> attributeRespone = new HashSet<>();
+        attributeRespone.add(attributeServiceImpl.getAttributeById(id));
+        return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
+    }
 
-	@PutMapping("attributes/{id}")
-	public ResponseEntity<DataResponse<AttributeResponse>> updateAttribute(@PathVariable String id,
-			@RequestBody AttributeRequest request) {
-		Set<AttributeResponse> attributeRespone = new HashSet<>();
-		attributeRespone.add(attributeServiceImpl.updateAttribute(id, request));
-		return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
-	}
+    @PostMapping
+    public ResponseEntity<DataResponse<AttributeResponse>> creatAttribute(@RequestBody AttributeRequest request) {
+        Set<AttributeResponse> attributeRespone = new HashSet<>();
+        attributeRespone.add(attributeServiceImpl.createAttribute(request));
+        return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
+    }
 
-	@DeleteMapping("attributes/{id}")
-	public ResponseEntity<DataResponse<AttributeResponse>> deleteAttribute(@PathVariable String id
-			) {
-		Set<AttributeResponse> attributeRespone = new HashSet<>();
-		attributeRespone.add(attributeServiceImpl.deleteAttribute(id));
-		return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
-	}
+    @PutMapping("attributes/{id}")
+    public ResponseEntity<DataResponse<AttributeResponse>> updateAttribute(@PathVariable String id,
+                                                                           @RequestBody AttributeRequest request) {
+        Set<AttributeResponse> attributeRespone = new HashSet<>();
+        attributeRespone.add(attributeServiceImpl.updateAttribute(id, request));
+        return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
+    }
 
-	@PostMapping("/productvariant")
-	public ResponseEntity<DataResponse<Object>> createAttributeValueVariant(@RequestParam String provariant, @RequestBody List<AttributeValueRequest> request) {
-		attributeServiceImpl.createValueProductVariant(provariant,request);
-		return ResponseEntity.ok(DataResponse.<Object>builder().message("Ok").build());
-	}
+    @DeleteMapping("attributes/{id}")
+    public ResponseEntity<DataResponse<AttributeResponse>> deleteAttribute(@PathVariable String id
+    ) {
+        Set<AttributeResponse> attributeRespone = new HashSet<>();
+        attributeRespone.add(attributeServiceImpl.deleteAttribute(id));
+        return ResponseEntity.ok(DataResponse.<AttributeResponse>builder().data(attributeRespone).build());
+    }
 
-	@GetMapping("/atributevalue/{name}")
-	public ResponseEntity<DataResponse<List<ValueResponse>>> getValueByNameAtri(@PathVariable String name){
-		Collection<List<ValueResponse>> result = new ArrayList<>();
-		result.add(attributeServiceImpl.getValueByNameAtri(name));
-		return ResponseEntity.ok(DataResponse.<List<ValueResponse>>builder().data(result).build());
-	}
+    @PostMapping("/productvariant")
+    public ResponseEntity<DataResponse<Object>> createAttributeValueVariant(@RequestParam String provariant, @RequestBody List<AttributeValueRequest> request) {
+        attributeServiceImpl.createValueProductVariant(provariant, request);
+        return ResponseEntity.ok(DataResponse.<Object>builder().message("Ok").build());
+    }
+
+    @GetMapping("/atributevalue/{name}")
+    public ResponseEntity<DataResponse<ValueResponse>> getValueByNameAtri(@PathVariable String name) {
+        List<ValueResponse> valueResponses = attributeServiceImpl.getValueByNameAtri(name);
+        return ResponseEntity.ok(DataResponse.<ValueResponse>builder().data(valueResponses).build());
+    }
 }
--- src/main/java/iuh/fit/se/techgalaxy/dto/response/ProductResponse.java
+++ src/main/java/iuh/fit/se/techgalaxy/dto/response/ProductResponse.java
@@ -1,5 +1,6 @@
 package iuh.fit.se.techgalaxy.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -8,7 +9,9 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductResponse {
      String id;
      String name;
+     TrademarkResponse trademark;
 }
