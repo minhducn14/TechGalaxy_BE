084f5aa819f46966fffbc0f7b75bcda7d756d208
PhamVanThanh <phamtrung21112003@gmail.com>
2024-11-28 23:38:32 +0700

    constraint CustomerRequest

--- src/main/java/iuh/fit/se/techgalaxy/dto/request/CustomerRequest.java
+++ src/main/java/iuh/fit/se/techgalaxy/dto/request/CustomerRequest.java
@@ -3,6 +3,8 @@ package iuh.fit.se.techgalaxy.dto.request;
 import iuh.fit.se.techgalaxy.entities.Account;
 import iuh.fit.se.techgalaxy.entities.enumeration.CustomerStatus;
 import iuh.fit.se.techgalaxy.entities.enumeration.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -17,7 +19,10 @@ import java.time.LocalDateTime;
 public class CustomerRequest {
     String id;
     CustomerStatus userStatus;
+    @NotBlank(message = "NAME_NOT_EMPTY")
     String name;
+    @NotBlank(message = "PHONE_NOT_EMPTY")
+    @Pattern(regexp = "^0[0-9]{9}$", message = "PHONE_INVALID")
     String phone;
     Gender gender;
     String avatar;
